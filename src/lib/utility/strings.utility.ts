655fd91667e14686f0414a7258913a1438076562
Luke Burke <51155288+LukeBurke99@users.noreply.github.com>
2025-03-16 04:30:57 +0000

    splitCamelCase function and some config changes

--- src/lib/utility/strings.utility.ts
+++ src/lib/utility/strings.utility.ts
@@ -6,3 +6,12 @@
 export function capitalize(str: string): string {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+/**
+ * Split a camel case string into a space separated string
+ * @param str The string to split
+ * @returns The split string
+ */
+export function splitCamelCase(str: string): string {
+	return str.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
